6e79564798c808aefadef9ac4c7d86f134e16225
Madirex <angel@madirex.com>
2025-06-26 00:01:04 +0200

    feat: debug print implementation

---
+++ src/debug/debug_print.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "../../includes/cub3d.h"
+
+void print_textures_debug(t_cub3d *cub)
+{
+	printf("[Debug] Textures:\n");
+	if (cub->textures.no)
+		printf("NO: %s\n", cub->textures.no);
+	if (cub->textures.so)
+		printf("SO: %s\n", cub->textures.so);
+	if (cub->textures.we)
+		printf("WE: %s\n", cub->textures.we);
+	if (cub->textures.ea)
+		printf("EA: %s\n", cub->textures.ea);
+	if (cub->textures.floor.r != -1)
+		printf("Floor: %d,%d,%d\n", cub->textures.floor.r, cub->textures.floor.g, cub->textures.floor.b);
+	if (cub->textures.ceiling.r != -1)
+		printf("Ceiling: %d,%d,%d\n", cub->textures.ceiling.r, cub->textures.ceiling.g, cub->textures.ceiling.b);
+}
